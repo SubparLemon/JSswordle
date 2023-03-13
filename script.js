57e4e20ab5f0d893dcc907cd5f49540f16683c62
Jonathan <jshi25@bfhsla.org>
2023-03-13 10:57:04 -0500

    removed alerts

--- script.js
+++ script.js
@@ -1,4 +1,74 @@
 window.addEventListener("load", () => {
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+
+// Set canvas dimensions
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+// Create an array to store the confetti particles
+    const confetti = [];
+
+// Function to create a confetti particle
+    function createConfetti() {
+        // Generate random values for the confetti particle
+        const size = Math.random() * 10 + 8;
+        const x = Math.random() * canvas.width;
+        const y = Math.random() * canvas.height / 2;
+        const angle = Math.random() * Math.PI / 1.2;
+        const speed = Math.random() * 3 + 1;
+        const color = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
+
+        // Add the confetti particle to the array
+        confetti.push({ size, x, y, angle, speed, color });
+    }
+
+// Function to draw a confetti particle
+    function drawConfetti(particle) {
+        ctx.beginPath();
+        ctx.arc(particle.x, particle.y, particle.size, 0, Math.PI * 2);
+        ctx.fillStyle = particle.color;
+        ctx.fill();
+    }
+
+// Function to update the confetti particles
+    function updateConfetti() {
+        // Create new confetti particles at a random interval
+        if (Math.random() > 0.9) {
+            createConfetti();
+        }
+
+        // Loop through the confetti particles and update their position and angle
+        for (let i = 0; i < confetti.length; i++) {
+            const particle = confetti[i];
+
+            // Update the position based on the angle and speed
+            particle.x += Math.cos(particle.angle) * particle.speed;
+            particle.y += Math.sin(particle.angle) * particle.speed;
+
+            // Add gravity to the particle's speed
+            particle.speed += 0.2;
+
+            // Remove the particle if it goes off the screen
+            if (particle.y > canvas.height) {
+                confetti.splice(i, 1);
+                i--;
+            }
+        }
+    }
+
+// Function to animate the confetti particles
+    function animateConfetti() {
+        // Clear the canvas
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        // Update and draw the confetti particles
+        updateConfetti();
+        confetti.forEach(drawConfetti);
+
+        // Call the function again to create an animation loop
+        requestAnimationFrame(animateConfetti);
+    }
     /////// INIT //////
     console.log("display.js loaded");
     //the row size CSS for boxes is NOT dynamic, so if you want to change these values you'll have to change that too
@@ -37,19 +107,25 @@ window.addEventListener("load", () => {
     // boxID: in [xy] format
     //color: hex value
     function changeColor(boxID, color){
-        $("#box"+boxID).css("background-color", "#"+color);
+        $("#box"+boxID).css("background-color", color);
     }
     //actual code
     addBoxes();
-
+    function dispTextSet() {
+        ctx.font = "50px Arial";
+        ctx.fillStyle = "white";
+        ctx.textAlign = "center";
+        ctx.fillRect(canvas.width / 2 - 200, canvas.height / 2 - 67, 400, 100)
+        ctx.fillStyle = "black";
+    }
     function compareWord(word, userWord, row) {
         for (i = 0; i < word.length; i++) {
             if (word.slice(i, i+1) === userWord.slice(i, i+1)) {
-                changeColor(row.toString()+(i.toString()), "00FF00")
+                changeColor(row.toString()+(i.toString()), "#05ab05")
             } else if (word.includes(userWord.slice(i, i+1))) {
-                changeColor(row.toString()+(i.toString()), "FFFF00")
+                changeColor(row.toString()+(i.toString()), "#FFFF00")
             } else {
-                changeColor(row.toString()+(i.toString()), "FF0000")
+                changeColor(row.toString()+(i.toString()), "#FF0000")
             }
         }
     }
@@ -61,8 +137,31 @@ window.addEventListener("load", () => {
     function dispOut(word, row) {
         clearRow(row);
         for (i = 0; i < word.length; i++) {
-            console.log(i.toString());
             addCharacter(row.toString()+(i.toString()), word.substring(i,i+1));
+        }
+    }
+
+    function check() {
+        if (!(lost)) {
+            if (userInput.length !== 6) {
+                dispTextSet()
+                ctx.fillText("Not 6 letters", canvas.width / 2, canvas.height / 2);
+            } else if (!(words.includes(userInput.toLowerCase()))) {
+                dispTextSet()
+                ctx.fillText("Not in word list", canvas.width / 2, canvas.height / 2);
+            } else if (userInput === wordleWord) {
+                compareWord(wordleWord, userInput, row)
+                animateConfetti()
+            } else {
+                compareWord(wordleWord, userInput, row)
+                row++;
+                userInput = "";
+                if (row === 6) {
+                    dispTextSet()
+                    ctx.fillText("You Lost :(", canvas.width / 2, canvas.height / 2);
+                    lost = true;
+                }
+            }
         }
     }
 
@@ -76,12 +175,12 @@ window.addEventListener("load", () => {
     request.open("GET", "./dictionaryUsedWords.json", false);
     request.send(null)
     let wordsUsed = Object.keys(JSON.parse(request.responseText));
-
     // gets a random word from the array
     let wordleWord = wordsUsed[Math.floor(Math.floor(Math.random()*wordsUsed.length))].toUpperCase();
     let userInput = "";
     let row = 0;
     let darkMode = false;
+    let lost = false;
     console.log(wordleWord);
     // checks when key pressed, filters input, and modifies userinput
     window.addEventListener("keydown", function (event) {
@@ -89,27 +188,18 @@ window.addEventListener("load", () => {
             userInput += event.key.toUpperCase();
             dispOut(userInput, row);
         } else if (event.key === "Backspace") {
-            console.log("backspace");
-            userInput = userInput.substring(0, userInput.length - 1);
+            if (!(lost)) {
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                userInput = userInput.substring(0, userInput.length - 1);
+            }
             dispOut(userInput, row);
+        } else if (event.key === "Enter") {
+            check();
         }
     }, true);
     //checks user word
     document.getElementById('button').onclick = function() {
-        if (!(words.includes(userInput.toLowerCase()))) {
-            alert("not a real word")
-        } else if (userInput === wordleWord) {
-            compareWord(wordleWord, userInput, row)
-        } else if (userInput.length !== 6) {
-            alert("Must be a 6 letter word");
-        } else {
-            compareWord(wordleWord, userInput, row)
-            row++;
-            userInput = "";
-            if (row === 6) {
-                alert("you lost")
-            }
-        }
+        check();
     };
 
     document.getElementById('darkmode').onclick = function () {
@@ -117,7 +207,7 @@ window.addEventListener("load", () => {
         if (darkMode) {
             $("body").css("background-color", "#ffffff");
             $("button").css("background-color", "#383838");
-            $("p").css("color", "#000000");
+            $("#desc").css("color", "#000000");
             $("h1").css("color", "#000000");
             $("h2").css("color", "#000000");
             $("h3").css("color", "#000000");
@@ -126,7 +216,7 @@ window.addEventListener("load", () => {
             $("body").css("background-color", "#1e1e1e");
             $("button").css("background-color", "#eaeaea");
             $("button").css("color", "#000000");
-            $("p").css("color", "#ffffff");
+            $("#desc").css("color", "#ffffff");
             $("h1").css("color", "#ffffff");
             $("h2").css("color", "#ffffff");
             $("h3").css("color", "#ffffff");
